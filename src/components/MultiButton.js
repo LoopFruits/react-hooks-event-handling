a9132d123b46b7ad8de84063b6f8e90e444e33b1
LoopFruits <Kensworldc@gmail.com>
2022-04-23 01:06:52 -0400

    on click section complete

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -4,12 +4,12 @@ function MultiButton() {
   function handleClick(number) {
     console.log(`Button ${number} was clicked`);
   }
-
+// an arrow function needs to be added here because
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
 }
--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
+
+  function tickle() { // this is the event function
     console.log("Teehee!");
   }
+    //calls our tickle function?
+  return <button onClick={tickle}>Tickle me!</button>;
 
-  return <button>Tickle me!</button>;
+  // this can also  eliminate the need a of writing more code by using an arrow function over an actual function
+  // function Tickler() {
+  //   return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>;
+  // }
 }
 
 export default Tickler;
